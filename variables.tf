10eb789328ba6876c27f9114a2f838a89be630e5
ssahu11 <110920734+ssahu11@users.noreply.github.com>
2022-12-19 20:43:02 +0530

    created variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "test1"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "my-security-group"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "Test_instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ec2 instance"
+  default     = "ami-0e6329e222e662a52"
+}
