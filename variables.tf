795d146a1feb1e060b5cd411f695a2f928899239
ssahu11 <110920734+ssahu11@users.noreply.github.com>
2022-12-21 18:26:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-security-group"
+  default     = "default"
 }
 
 variable "tag_name" {
