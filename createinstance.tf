0c5926aa1a569008e1b6708e4532d2697f280753
ssahu11 <110920734+ssahu11@users.noreply.github.com>
2022-12-21 18:36:50 +0530

    Update createinstance.tf

--- createinstance.tf
+++ createinstance.tf
@@ -1,16 +1,6 @@
 provider "aws" {
   region = var.aws_region
 }
-
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-name        = var.security_group
-description = "security group for Ec2 instance"
-tags= {
-    Name = var.security_group
-  }
-}
-
 # Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
